88230cf0c840f28687bb199355ad48d4c709bade
Gysel <3247575858@qq.com>
2026-04-20 22:40:32 +0800

    leetcode第22题

--- leetcode/src/leetcode_22/Solution.java
+++ leetcode/src/leetcode_22/Solution.java
@@ -5,11 +5,27 @@ import java.util.List;
 
 class Solution {
     /*括号生成*/
+    List<String> res=new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        List<String> res=new ArrayList<>();
-        /***
-         * 具体代码实现
-         */
+        dfs(n,n,"");
         return res;
+    }
+
+    /**
+     * @param left:左括号的数量
+     * @param right：右括号的数量
+     * @param curStr
+     */
+    private void dfs(int left,int right,String curStr){
+        if(left==0&&right==0){
+            res.add(curStr);
+            return;
+        }
+        if(left>0){
+            dfs(left-1,right,curStr+'(');
+        }
+        if(right>left){
+            dfs(left,right-1,curStr+')');
+        }
     }
 }
