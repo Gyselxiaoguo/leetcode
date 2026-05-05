d887e0b67e735e0fa9381320647a2644cb4e43f0
Gysel <3247575858@qq.com>
2026-05-05 22:37:16 +0800

    leetcode第79、131题

---
+++ leetcode/src/leetcode_131/Solution.java
@@ -0,0 +1,40 @@
+package leetcode_131;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<List<String>> res=new ArrayList<>();
+    List<String> path=new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        backtrack(s,0);
+        return res;
+    }
+    private void backtrack(String s ,int start){
+        //结束条件
+        if(start==s.length()){
+            //切完了
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        //决策：尝试切 1,2,... 长度的子串
+        for(int i=start;i<s.length();i++){
+            //切取子串
+            String sub = s.substring(start, i + 1);
+            //1.剪枝
+            if(!isPalindrome(sub)){
+                continue;
+            }
+            //2.做选择
+            path.add(sub);
+            //3.递归
+            backtrack(s,i+1);
+            //4.回溯
+            path.remove(path.size()-1);
+        }
+    }
+    private boolean isPalindrome(String s){
+        String newStr = new StringBuffer(s).reverse().toString();
+        return newStr.equals(s);
+    }
+}
---
+++ leetcode/src/leetcode_79/Solution.java
@@ -0,0 +1,41 @@
+package leetcode_79;
+
+class Solution {
+    char[][] myBoard;
+    char[] myWord;
+    boolean res=false;
+    int[][] move={{1,0},{0,-1},{-1,0},{0,1}};
+
+    public boolean exist(char[][] board, String word) {
+        int row=board.length;
+        int col=board[0].length;
+        myBoard=board;
+        myWord=word.toCharArray();
+        boolean[][] visited=new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                backtrack(i,j,0,visited);
+            }
+        }
+        return res;
+    }
+    private void backtrack(int row,int col ,int index,boolean[][] visited){
+        if(row<0||row>=myBoard.length||col<0||col>=myBoard[0].length
+                ||visited[row][col]||res||myBoard[row][col]!=myWord[index]){
+            return;
+        }
+        if(index==myWord.length-1){
+            res=true;
+            return;
+        }
+        visited[row][col]=true;
+        for (int i = 0; i < move.length; i++) {
+            row+=move[i][0];
+            col+=move[i][1];
+            backtrack(row, col, index+1, visited);
+            row-=move[i][0];
+            col-=move[i][1];
+        }
+        visited[row][col]=false;
+    }
+}
